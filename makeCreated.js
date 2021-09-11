22c2c9ba70106c0f1898b050b5c0f2b459013777
Taisuke Fukuno <fukuno@jig.jp>
2021-09-11 21:39:33 +0900

    add 総廃業ダッシュボード

--- makeCreated.js
+++ makeCreated.js
@@ -32,17 +32,39 @@ export const makeCreated = async (date) => {
   });
   //console.log(data[0]);
 
+  /* // process.csv
+  name,label
+  01,新規
+  11,商号又は名称の変更
+  12,国内所在地の変更
+  13,国外所在地の変更
+  21,登記記録の閉鎖等
+  22,登記記録の復活等
+  71,吸収合併 // 存続会社が記載されている
+  72,吸収合併無効
+  81,商号の登記の抹消
+  99,削除
+  */
   // filter
   const data2 = data.filter((d) => d.process == "新規" && d.correct == 0);
   await Deno.writeTextFile(
     "data/diff_" + date + "_created.csv",
     CSV.stringify(data2),
   );
+  const data3 = data.filter((d) => (d.process == "登記記録の閉鎖等" || d.process == "商号の登記の抹消" || d.process == "削除") && d.correct == 0);
+  await Deno.writeTextFile(
+    "data/diff_" + date + "_terminated.csv",
+    CSV.stringify(data3),
+  );
 };
 
-if (Deno.mainModule.endsWith("/created.js")) {
+if (Deno.mainModule.endsWith("/makeCreated.js")) {
   //const date = "20210908";
   //const date = "20210909";
   const date = "20210910";
   await makeCreated(date);
+  for (let i = 6; i < 10; i++) {
+    const date = "2021090" + i;
+    await makeCreated(date);
+  }
 }
