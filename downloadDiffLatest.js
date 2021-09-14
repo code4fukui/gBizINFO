4af1db7cd414b0dad868e6152d46c8d81a62dcd4
Taisuke Fukuno <fukuno@jig.jp>
2021-09-14 16:33:20 +0900

    update data

--- downloadDiffLatest.js
+++ downloadDiffLatest.js
@@ -9,11 +9,12 @@ import { Day } from "https://js.sabae.cc/DateTime.js";
 //await scrapeDiff();
 
 const token = "f83622bb-6551-440a-95b4-1192ff13fb5a"; // いつまで有効?
+//const token = "f075ad01-bf30-444f-abbc-4b7cfc11be02";
 
 const list = CSV.toJSON(await CSV.fetch("data/diff.csv"));
 const lastid = list[list.length - 1].fileNo;
-console.log(lastid);
-const nextid = lastid + 3; // 固定?
+const nextid = parseInt(lastid) + 3; // 固定?
+console.log(lastid, nextid);
 
 const url =
   "https://www.houjin-bangou.nta.go.jp/download/sabun/index.html?event=download&selDlFileNo=" +
