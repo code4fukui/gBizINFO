d48f5c527cd7baa8b9114b1a36dd09eb260b9919
Taisuke Fukuno <fukuno@jig.jp>
2021-10-04 17:02:08 +0900

    update sabae company

--- makeByCity.js
+++ makeByCity.js
@@ -3,8 +3,8 @@ import { CSV } from "https://js.sabae.cc/CSV.js";
 
 const gbiz = new GBizINFO();
 
-//const code = 18207; // 鯖江市
-const code = 27140; // 堺市
+const code = 18207; // 鯖江市
+//const code = 27140; // 堺市
 const data = await gbiz.getBasicByCityID(code);
 data.sort(gbiz.filterByCorporateID);
 console.log(JSON.stringify(data, null, 2), data?.length);
