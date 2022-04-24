d907cc4d3b1e27d2c5369653bf58891b8df622e0
Taisuke Fukuno <fukuno@jig.jp>
2022-04-25 06:51:52 +0900

    add fetchInfoSummary

--- fetchInfo.js
+++ fetchInfo.js
@@ -1,3 +1,5 @@
+import { ZenkakuAlpha } from "https://code4fukui.github.io/mojikiban/ZenkakuAlpha.js";
+
 const api = async (path, req) => {
   const token = Deno.env.get("GBIZ_ACCESS_TOKEN");
   const opt = {
@@ -25,8 +27,8 @@ export const fetchInfo = async (cid, outpath) => {
     return false;
   } catch (e) {
   }
-  const data = await api("hojin/" + cid);
-  console.log(data);
+  //const data = await api("hojin/" + cid);
+  //console.log(data);
   let res = null;
   //const list = ["", "certification", "commendation", "finance", "patent", "procurement", "subsidy", "workplace"];
   const list = ["certification", "commendation", "finance", "patent", "procurement", "subsidy", "workplace"];
@@ -40,6 +42,55 @@ export const fetchInfo = async (cid, outpath) => {
       res[name] = data["hojin-infos"][0][name];
     }
   }
-  await Deno.writeTextFile(fn, JSON.stringify(res, null, 2));
-  return true;
+  if (outpath) {
+    await Deno.writeTextFile(fn, JSON.stringify(res, null, 2));
+    return true;
+  }
+  return res;
+};
+
+export const fetchInfoSummary = async (cid) => {
+  const addUnique = (array, s) => {
+    if (array.indexOf(s) == -1) {
+      array.push(s);
+    }
+  };
+  const limit10 = (array) => {
+    const res = [];
+    for (let i = 0; i < Math.max(array.length, 10); i++) {
+      res.push(array[i]);
+    }
+    return  ZenkakuAlpha.toHan(res.join("、"));
+  }
+
+  const info = await fetchInfo(cid);
+  const patent = []; // 特許
+  const design = []; // 意匠
+  const trademark = []; // 商標
+  info.patent.sort((a, b) => b.application_date.localeCompare(a.application_date));
+  for (const d of info.patent) {
+    switch (d.patent_type) {
+      case "特許": {
+        addUnique(patent, d.title);
+        break;
+      }
+      case "意匠": {
+        addUnique(design, d.title);
+        break;
+      }
+      case "商標": {
+        addUnique(trademark, d.title);
+        break;
+      }
+      default: {
+        throw "unknown patent_type";
+      }
+    }
+    //console.log(d.application_date, d.title);
+  }
+  return {
+    特許: limit10(patent),
+    意匠: limit10(design),
+    商標: limit10(trademark),
+  };
 };
---
+++ fetchInfo_sample.js
@@ -0,0 +1,51 @@
+import { fetchInfo, fetchInfoSummary } from "./fetchInfo.js";
+
+// 2020年までしか入ってない？
+
+const cidjig = "3011101042092";
+const cidsharp = "6120001005484"; // 特許情報 22644件
+const cidnintendo = "1130001011420";
+//const cid = cidsharp;
+//const cid = cidjig;
+const cid = cidnintendo;
+
+const info = await fetchInfoSummary(cid);
+console.log(info);
+Deno.exit(0);
+
+
+//const res = await fetchInfo(cid, "temp/");
+const res = JSON.parse(await Deno.readTextFile("temp/" + cid + ".json"));
+//console.log(res);
+
+const patent = []; // 特許
+const design = []; // 意匠
+const trademark = []; // 商標
+const addUnique = (array, s) => {
+    if (array.indexOf(s) == -1) {
+        array.push(s);
+    }
+};
+res.patent.sort((a, b) => b.application_date.localeCompare(a.application_date));
+
+for (const d of res.patent) {
+    switch (d.patent_type) {
+        case "特許": {
+            addUnique(patent, d.title);
+            break;
+        }
+        case "意匠": {
+            addUnique(design, d.title);
+            break;
+        }
+        case "商標": {
+            addUnique(trademark, d.title);
+            break;
+        }
+        default: {
+            throw "unknown patent_type";
+        }
+    }
+    console.log(d.application_date, d.title);
+}
+//console.log(patent.join(", "), design.join(", "), trademark.join(", "));
